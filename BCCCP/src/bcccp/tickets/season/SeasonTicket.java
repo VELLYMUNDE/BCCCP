f2dc161fc9223b07e1bad70df5c7d140b2dacc42
manjot7721 <30396993+manjot7721@users.noreply.github.com>
2017-08-24 21:34:34 +1000

    Add files via upload

---
+++ BCCCP/src/bcccp/tickets/season/SeasonTicket.java
@@ -0,0 +1,83 @@
+package bcccp.tickets.season;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SeasonTicket implements ISeasonTicket {
+	
+	private List<IUsageRecord> usages;
+	private IUsageRecord currentUsage = null;
+	
+	private String ticketId;
+	private String carparkId;
+	private long startValidPeriod;
+	private long endValidPeriod;
+	
+	public SeasonTicket (String ticketId, 
+			             String carparkId, 
+			             long startValidPeriod,
+			             long endValidPeriod) {
+		//TDO Implement constructor
+		this.ticketId = ticketId;
+		this.carparkId = carparkId;
+		this.startValidPeriod = startValidPeriod;
+		this.endValidPeriod = endValidPeriod;
+	}
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return ticketId;
+	}
+
+	@Override
+	public String getCarparkId() {
+		// TODO Auto-generated method stub
+		return carparkId;
+	}
+
+	@Override
+	public long getStartValidPeriod() {
+		// TODO Auto-generated method stub
+		return startValidPeriod;
+	}
+
+	@Override
+	public long getEndValidPeriod() {
+		// TODO Auto-generated method stub
+		return endValidPeriod;
+	}
+
+	@Override
+	public boolean inUse() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void recordUsage(IUsageRecord record) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IUsageRecord getCurrentUsageRecord() {
+		// TODO Auto-generated method stub
+		return currentUsage;
+	}
+
+	@Override
+	public void endUsage(long dateTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<IUsageRecord> getUsageRecords() {
+		// TODO Auto-generated method stub
+		return usageRecord;
+	}
+
+
+}
