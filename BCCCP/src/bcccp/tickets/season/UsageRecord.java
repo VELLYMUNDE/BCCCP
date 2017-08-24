e510c19f634341830e05fb5d58ed1c332699cb92
manjot7721 <30396993+manjot7721@users.noreply.github.com>
2017-08-24 21:36:48 +1000

    Add files via upload

---
+++ BCCCP/src/bcccp/tickets/season/UsageRecord.java
@@ -0,0 +1,51 @@
+package bcccp.tickets.season;
+
+public class UsageRecord implements IUsageRecord {
+	
+	String ticketId;
+	long startDateTime;
+	long endDateTime;
+	
+	
+	
+	public UsageRecord(String ticketId, long startDateTime, long endDateTime) {
+		//TODO Implement constructor
+		this.ticketId = ticketId;
+		this.startDateTime = startDateTime;
+	}
+
+
+
+	@Override
+	public void finalise(long endDateTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public long getStartTime() {
+		// TODO Auto-generated method stub
+		return startDateTime;
+	}
+
+
+
+	@Override
+	public long getEndTime() {
+		// TODO Auto-generated method stub
+		return endDateTime;
+	}
+
+
+
+	@Override
+	public String getSeasonTicketId() {
+		// TODO Auto-generated method stub
+		return ticketID;
+	}
+	
+	
+	
+}
